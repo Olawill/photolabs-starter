6f341919825953c60b2d160606899c34340437e8
Olawill <henry.williams658@gmail.com>
2023-10-04 20:28:01 -0600

    Render PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,21 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
   /* Insert React */
+  return (
+    <div className="photo-list__item" key={props.sampleDataForPhotoListItem.id}>
+      <img className="photo-list__image" src={props.sampleDataForPhotoListItem.imageSource} alt={props.sampleDataForPhotoListItem.id} />
+
+      <div className="photo-list__user-details">
+        <img className="photo-list__user-profile" src={props.sampleDataForPhotoListItem.profile} alt={props.sampleDataForPhotoListItem.username} />
+        <div>
+          <p className="photo-list__user-info">{props.sampleDataForPhotoListItem.username}</p>
+          <p className="photo-list__user-location">{props.sampleDataForPhotoListItem.location.city}, {props.sampleDataForPhotoListItem.location.country}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
