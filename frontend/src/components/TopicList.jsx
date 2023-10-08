212e77d759fcd82d530c78fcb6d4f25738472dbc
Olawill <henry.williams658@gmail.com>
2023-10-07 19:48:07 -0600

    Refactor the app to use mock data

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,30 +2,31 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
+import topics from "./../../src/mocks/topics";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+// const sampleDataForTopicList = [
+//   {
+//     id: "1",
+//     slug: "topic-1",
+//     title: "Nature",
+//   },
+//   {
+//     id: "2",
+//     slug: "topic-2",
+//     title: "Travel",
+//   },
+//   {
+//     id: "3",
+//     slug: "topic-3",
+//     title: "People",
+//   },
+// ];
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
       {/* Insert React */}
-      {sampleDataForTopicList.map(topic => <TopicListItem key={topic.id} topic={topic} />)}
+      {topics.map(topic => <TopicListItem key={topic.id} topic={topic} />)}
     </div>
   );
 };
