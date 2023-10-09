ca715e8257d4b5ee3e1aff77a4a8be4acfb13aca
Olawill <henry.williams658@gmail.com>
2023-10-09 16:32:22 -0600

    Add model closing capability

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
+
 const PhotoDetailsModal = () => {
+  const [isActive, setIsActive] = useState(true);
+
+  const handleClose = (e) => {
+    e.preventDefault();
+    setIsActive(false);
+  };
+
   return (
-    <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
+    <div className="photo-details-modal"
+      style={{display: isActive ? 'inherit' : 'none'}}
+    >
+      <button className="photo-details-modal__close-button" onClick={handleClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
     </div>
